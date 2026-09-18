8897c8d7a37824819cbd7147c20d7ce940266319
unknown <abhikr200125#gmail.com>
2026-09-18 22:39:56 +0530

    Add env

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,23 @@
-//const { Pool } = require('pg');
-//require('dotenv').config();
-
-
 const { Pool } = require('pg');
+require('dotenv').config();
+
+//
+//const { Pool } = require('pg');
+//
+//const pool = new Pool({
+//  host: process.env.DB_HOST,
+//  port: process.env.DB_PORT,
+//  database: process.env.DB_NAME,
+//  user: process.env.DB_USER,
+//  password: process.env.DB_PASSWORD,
+//
+//  ssl:
+//    process.env.DB_SSL === 'true'
+//      ? { rejectUnauthorized: false }
+//      : false,
+//});
+
+
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -11,26 +26,11 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 
-  ssl:
-    process.env.DB_SSL === 'true'
-      ? { rejectUnauthorized: false }
-      : false,
+  // Neon PostgreSQL requires SSL
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
-module.exports = pool;
-
-//const pool = new Pool({
-//  host: process.env.DB_HOST,
-//  port: process.env.DB_PORT,
-//  database: process.env.DB_NAME,
-//  user: process.env.DB_USER,
-//  password: process.env.DB_PASSWORD,
-//
-//  // Neon PostgreSQL requires SSL
-//  ssl: {
-//    rejectUnauthorized: false,
-//  },
-//});
 
 pool.on('connect', () => {
   console.log('PostgreSQL connected');
